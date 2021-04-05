1da00d01cf4713803a7cbfc5f8ad0bfe6ee5405c
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-04-05 00:24:36 -0700

    sass

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './homepage.styles.scss';
 const HomePage = () => {
     return(
        <div className='homePage'>
@@ -7,6 +7,41 @@ const HomePage = () => {
                 <div className='menu-item'>
                     <div className='content'>
                         <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>MENS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>JACKETS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>WOMENS</h1>
                         <span className='subtitle'>SHOP NOW</span>
                     </div>
                 </div>
