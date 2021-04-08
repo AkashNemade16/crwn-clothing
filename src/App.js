0ff8217dba632b87e68d3b7413e389d8aa536d3f
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-04-07 22:37:47 -0700

    withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import HomePage from'./pages/homepage/homepage.component';
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
-    <div >
-      <HomePage/>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/hats' component={HatsPage} />
+        </Switch>
+      </BrowserRouter>  
     </div>
   );
 }
